c739571ec477e6333d55987041e42c9afd50ab13
kevin-nkw <kwng.kevin@gmail.com>
2018-04-26 16:29:17 +0800

    footer update

---
+++ js/renderComponents.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('#navi').load('assets/components/nav.html');
+    $('#foot').load('assets/components/footer.html');
+
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 54)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+});
