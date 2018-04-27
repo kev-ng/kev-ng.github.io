3fdd5a831da09864f3b219ecbc2b9afd9b01e282
kevin-nkw <kwng.kevin@gmail.com>
2018-04-27 22:56:00 +0800

    content update and nav fix

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,15 @@
 
+  // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 50) {
-      $("#mainNav").addClass("navbar-shrink");
+    if ($(document).scrollTop() > 50) {
+      $("nav").addClass("navbar-shrink");
     } else {
-      $("#mainNav").removeClass("navbar-shrink");
+      $("nav").removeClass("navbar-shrink");
     }
   };
+  
+  // Collapse now if page is not at top
   navbarCollapse();
+
+  // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
