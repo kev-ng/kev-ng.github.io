0b43d3cfd9cfdef6899e66982fb8dff4978263cd
kevin-nkw <kwng.kevin@gmail.com>
2018-04-27 23:00:57 +0800

    nav bar animation fix

--- js/navbar.js
+++ js/navbar.js
@@ -13,3 +13,22 @@
 
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
+
+  // Smooth scrolling using jQuery easing
+$('a.nav-link[href*="#"]:not([href="#"])').click(function () {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+          $('html, body').animate({
+              scrollTop: (target.offset().top)
+          }, 500);
+          return false;
+      }
+  }
+});
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.nav-link').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
--- js/renderComponents.js
+++ js/renderComponents.js
@@ -5,24 +5,5 @@ $(document).ready(function () {
 window.onload=function () {
     document.body.style.visibility = "visible";
 }
-
-// Smooth scrolling using jQuery easing
-$('a.nav-link[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: (target.offset().top - 54)
-            }, 500);
-            return false;
-        }
-    }
 });
-});
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.nav-link').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
 
