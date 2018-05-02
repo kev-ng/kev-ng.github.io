eb70cb2aa5b7089d676b78f52e9a358d52c5934b
kevin-nkw <kwng.kevin@gmail.com>
2018-05-02 19:07:20 +0800

    loading update

--- js/renderComponents.js
+++ js/renderComponents.js
@@ -2,19 +2,14 @@ $(document).ready(function () {
     $('#navi').load('assets/components/nav.html');
     $('#foot').load('assets/components/footer.html');
 
+
+
+});
+
 window.onload=function () {
-    var els= document.getElementsByClassName('action-link');
-    Array.prototype.forEach.call(els, function(el) {
-        // Do stuff here
-        el.style.visibility="visible";
-    });
 
     document.getElementById('content').style.visibility = "visible";
     document.getElementById('spinner').style.visibility="hidden";
     document.getElementById('spinner').style.display="none"
 
 }
-
-
-});
-
