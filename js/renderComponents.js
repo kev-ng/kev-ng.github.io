a115429e88ea5f5338084192dc5df0f15c53c878
kevin-nkw <kwng.kevin@gmail.com>
2018-05-02 18:02:46 +0800

    added loader

--- js/renderComponents.js
+++ js/renderComponents.js
@@ -3,7 +3,18 @@ $(document).ready(function () {
     $('#foot').load('assets/components/footer.html');
 
 window.onload=function () {
-    document.body.style.visibility = "visible";
+    var els= document.getElementsByClassName('action-link');
+    Array.prototype.forEach.call(els, function(el) {
+        // Do stuff here
+        el.style.visibility="visible";
+    });
+
+    document.getElementById('content').style.visibility = "visible";
+    document.getElementById('spinner').style.visibility="hidden";
+    document.getElementById('spinner').style.display="none"
+
 }
+
+
 });
 
