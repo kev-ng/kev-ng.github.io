4238279447298a1e160aaacb27b4e07309a805aa
kevin-nkw <kwng.kevin@gmail.com>
2018-05-04 15:13:39 +0800

    prevent scroll when loading

--- js/renderComponents.js
+++ js/renderComponents.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
     $('#navi').load('assets/components/nav.html');
     $('#foot').load('assets/components/footer.html');
-
+    $('html, body').css({
+        'overflow': 'hidden',
+        'height': '100%'
+      })
 
 
 });
@@ -11,5 +14,8 @@ window.onload=function () {
     document.getElementById('content').style.visibility = "visible";
     document.getElementById('spinner').style.visibility="hidden";
     document.getElementById('spinner').style.display="none"
-
+    $('html, body').css({
+        'overflow': 'auto',
+        'height': 'auto'
+      })
 }
